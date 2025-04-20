f7923205be2f132b707c375ca9ee58321ef3506c
Hend El-Mahdy <hendm8042@gmail.com>
2025-04-21 00:14:13 +0200

    Enhance LeetCode 448 solution to O(n) time and O(n) space

--- Array/448-FindDisappearedNumbers.swift
+++ Array/448-FindDisappearedNumbers.swift
@@ -1,11 +1,24 @@
 /*
  * Problem:- 448. Find All Numbers Disappeared in an Array
  * Link:- https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
- * Time: O(n log n)
- * Space: O(n)
  */
 
-class Solution {
+// SolutionOne with Time: O(n)  and  Space: O(n)
+class SolutionOne {
+    func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
+        let numbersSet = Set(nums)
+        var hiddenNumbers: [Int] = []
+        for i in 1...nums.count {
+            if !numbersSet.contains(i) {
+                hiddenNumbers.append(i)
+            }
+        }
+        return hiddenNumbers
+    }
+}
+
+// SolutionOne with Time: O(n log n)  and  Space: O(n) 
+class SolutionTwo {
     func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
         var hiddenNumbers: [Int] = []
         var shownNumbers = nums
